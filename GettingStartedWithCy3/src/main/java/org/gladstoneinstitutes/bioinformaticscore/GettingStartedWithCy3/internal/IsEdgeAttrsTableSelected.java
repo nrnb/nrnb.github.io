6f472e8893fb11a92a58f01e60403ec468a49f20
jeff <jeff@pinscher.ucsf.edu>
2013-11-12 10:45:46 -0800

    removing randomly copied prototype code

--- GettingStartedWithCy3/src/main/java/org/gladstoneinstitutes/bioinformaticscore/GettingStartedWithCy3/internal/IsEdgeAttrsTableSelected.java
+++
@@ -1,9 +0,0 @@
-package org.gladstoneinstitutes.bioinformaticscore.GettingStartedWithCy3.internal;
-
-import com.agilent.AdaptiveJavaHelp.Condition;
-
-public class IsEdgeAttrsTableSelected implements Condition {
-    public boolean isMet() {
-        return "Edge Attributes".equals(CytoPanelListener.selectedCytoPanel);
-    }
-}
--- GettingStartedWithCy3/src/main/java/org/gladstoneinstitutes/bioinformaticscore/GettingStartedWithCy3/internal/IsThereANetwork.java
+++
@@ -1,9 +0,0 @@
-package org.gladstoneinstitutes.bioinformaticscore.GettingStartedWithCy3.internal;
-
-import com.agilent.AdaptiveJavaHelp.Condition;
-
-public class IsThereANetwork implements Condition {
-    public boolean isMet() {
-        return Services.appMgr.getCurrentNetworkView() != null;
-    }
-}
