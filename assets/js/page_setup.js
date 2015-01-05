4ff743ac7df9da97c8313a456d5bcebc9f0ab340
khanspers <khanspers@gladstone.ucsf.edu>
2015-01-05 13:20:27 -0800

    updates copyright year to 2015

--- assets/js/page_setup.js
+++ assets/js/page_setup.js
@@ -137,7 +137,7 @@ var footerText =
                 'of General Medical Sciences (NIGMS) under grant P41 GM103504.' +
                 '</p>' +
                 '<p>' +
-                '&copy; NRNB, 2014 <i class="icon-envelope icon-white"></i> ' +
+                '&copy; NRNB, 2015 <i class="icon-envelope icon-white"></i> ' +
                 '<a href="mailto:apico@gladstone.ucsf.edu">email us</a>' +
                 '</p>' +
         '</div>' +
