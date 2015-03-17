b87d23167b04189d830a6669ed6769f8c6ed3ec0
AlexanderPico <apico@gladstone.ucsf.edu>
2015-03-17 15:56:22 -0700

    added collab wall

--- assets/js/page_setup.js
+++ assets/js/page_setup.js
@@ -118,6 +118,7 @@ var navbarText =
                         '<li><a href="academy.html">NRNB Academy</a></li>' +
                         '<li><a href="competitions.html">NRNB Competitions</a></li>' +
                         '<li><a href="collaboration-report.html">Ongoing Collaborations</a></li>' +
+			'<li><a href="collab-wall.html">Wall of Collaborators</a></li>' +
                         '<li><a href="testimonials.html#collab-tab">Testimonials</a></li>' +
                         '<li class="divider"></li>' +
                         '<li><a href="collaboration-tracking.html">Collaboration Request Form</a></li>' +
