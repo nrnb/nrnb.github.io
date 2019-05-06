b85f673e38b3700649dbc23a326d2b25a0ceb784
Kristina Hanspers <kristina.hanspers@gladstone.ucsf.edu>
2019-05-06 15:39:21 -0700

    removed link to ongoing collaborations

--- assets/js/page_setup.js
+++ assets/js/page_setup.js
@@ -105,7 +105,6 @@ var navbarText =
                         '<li><a href="gsoc.html">Google Summer of Code</a></li>' +
                         '<li><a href="gsoc.html">NRNB Academy</a></li>' +
                         '<li><a href="competitions.html">NRNB Competitions</a></li>' +
-                        '<li><a href="collaboration-report.html">Ongoing Collaborations</a></li>' +
 			'<li><a href="collab-wall.html">Wall of Collaborators</a></li>' +
                         '<li><a href="testimonials.html#collab-tab">Testimonials</a></li>' +
                         '<li class="divider"></li>' +
