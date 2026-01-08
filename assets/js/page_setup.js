036577052ad90eab4d5837ad8c8022ea2d951f0e
khanspers <kristina.hanspers@gladstone.ucsf.edu>
2026-01-08 10:07:16 -0800

    removed Alex info and contact

--- assets/js/page_setup.js
+++ assets/js/page_setup.js
@@ -126,8 +126,7 @@ var footerText =
                 'of General Medical Sciences (NIGMS) under grant P41 GM103504 from 2011-2025.' +
                 '</p>' +
                 '<p>' +
-                '&copy; NRNB, 2026 <i class="icon-envelope icon-white"></i> ' +
-                '<a href="mailto:alex.pico@gladstone.ucsf.edu">email us</a>' +
+                '&copy; NRNB, 2026' +
                 '</p>' +
         '</div>' +
     '</div>';
